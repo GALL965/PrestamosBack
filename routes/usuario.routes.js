8ec04026c59e9b661bfe309b5b92587cc61ee4b8
GALL965 <bonaisdecamote45@gmail.com>
2025-06-10 17:58:19 -0600

    g

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,29 +1,6 @@
 const express = require('express');
 const router = express.Router();
 
-// Importar todo desde usuario.controller.js en un solo bloque
-const {
-  crearUsuario,
-  obtenerUsuarios,
-  crearAdministrador,
-  obtenerAdministradores,
-  loginUsuario
-} = require('../controllers/usuario.controller');
-
-const validateUsuario = require('../middlewares/validateUsuario');
-
-// Rutas
-router.post('/', validateUsuario, crearUsuario);
-router.get('/', obtenerUsuarios);
-
-router.post('/admin', crearAdministrador);
-router.get('/admins', obtenerAdministradores);
-
-router.post('/login', loginUsuario);
-
-module.exports = router;
-
-
 const {
   crearUsuario,
   obtenerUsuarios,
@@ -33,5 +10,14 @@ const {
   obtenerEstudiantes
 } = require('../controllers/usuario.controller');
 
-// Nueva ruta:
-router.get('/estudiantes', obtenerEstudiantes);
+const validateUsuario = require('../middlewares/validateUsuario');
+
+// Rutas
+router.post('/', validateUsuario, crearUsuario);
+router.get('/', obtenerUsuarios);
+router.post('/admin', crearAdministrador);
+router.get('/admins', obtenerAdministradores);
+router.post('/login', loginUsuario);
+router.get('/estudiantes', obtenerEstudiantes); // esta es la nueva
+
+module.exports = router;
