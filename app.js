0267fb3a2753e986be0e5400c7ae6dacfd4a5fbe
GALL965 <bonaisdecamote45@gmail.com>
2025-06-10 18:45:01 -0600

    f

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const db = require('./config/database');
+
+const app = express();
+
+// Modelos
+const Prestamo = require('./models/prestamo.model');
+const Puntuacion = require('./models/puntuacion.model');
+
+// Rutas
 const prestamoRoutes = require('./routes/prestamo.routes');
 const usuarioRoutes = require('./routes/usuario.routes');
 const articuloRoutes = require('./routes/articulo.routes');
 const disponibilidadRoutes = require('./routes/disponibilidad.routes');
-const Prestamo = require('./models/prestamo.model');
-const app = express();
 const puntuacionRoutes = require('./routes/puntuacion.routes');
-
 
 // 🔥 CORS ABIERTO (para pruebas)
 app.use(cors());
 
+// Middleware JSON
 app.use(express.json());
 
-// Ruta
+// Usar rutas
 app.use('/api/puntuaciones', puntuacionRoutes);
 app.use('/api/prestamos', prestamoRoutes);
 app.use('/api/usuarios', usuarioRoutes);
@@ -30,13 +36,17 @@ db.authenticate()
   })
   .catch(err => console.error('❌ Error en conexión MySQL:', err));
 
-// Puerto dinámico para Render
+// Sincronizar modelos
+Prestamo.sync()
+  .then(() => console.log("🟢 Tabla 'prestamos' sincronizada."))
+  .catch(err => console.error("🔴 Error al sincronizar 'prestamos':", err));
+
+Puntuacion.sync()
+  .then(() => console.log("🟢 Tabla 'puntuaciones' sincronizada."))
+  .catch(err => console.error("🔴 Error al sincronizar 'puntuaciones':", err));
+
+// Iniciar servidor
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`🚀 Servidor corriendo en puerto ${PORT}`);
 });
-
-
-Prestamo.sync()
-  .then(() => console.log("🟢 Tabla 'prestamos' sincronizada."))
-  .catch(err => console.error("🔴 Error al sincronizar 'prestamos':", err));
